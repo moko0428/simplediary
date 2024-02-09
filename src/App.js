e96f17b1c0a6f19df6dd8c92d3172cb5b97f81b1
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-09 20:21:44 +0900

    일기 추가하기

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "moko",
-    content: "aa",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "12",
-    content: "aa",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "13",
-    content: "aa",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "15",
-    content: "aa",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "moko",
+//     content: "aa",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "12",
+//     content: "aa",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "13",
+//     content: "aa",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "15",
+//     content: "aa",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
