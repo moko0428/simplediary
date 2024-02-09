7b0aef93eccf1faaf45a0b16c39c38e325fecb36
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-09 20:33:36 +0900

    일기 삭제하기

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,10 +1,29 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({
+  onDelete,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
-        <span>
-          작성자 : {author} | 감정점수 : {emotion}
-        </span>
+        <div>
+          <span>
+            작성자 : {author} | 감정점수 : {emotion}
+          </span>
+          <button
+            onClick={() => {
+              console.log(id);
+              if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+                onDelete(id);
+              }
+            }}
+          >
+            삭제하기
+          </button>
+        </div>
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
